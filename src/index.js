5a740857d4a54082ab013276c5b754aa2f205a45
Pavel Nishcharetau <alzarlion@gmail.com>
2018-02-20 01:21:30 +0300

    Task warm up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  var fahrenheitAtZeroDegrees = 32;
+  var resultFahrenheit = 1.8;
+
+  if(temperature !== 0) {
+      resultFahrenheit *= temperature;
+      resultFahrenheit += fahrenheitAtZeroDegrees;
+  } else {
+      resultFahrenheit = 0;
+      resultFahrenheit += fahrenheitAtZeroDegrees;
+  }
+
+  return resultFahrenheit;
 };
